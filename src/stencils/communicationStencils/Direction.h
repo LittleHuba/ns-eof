00df183d1a5dcbff381906544edb3310c1bb753b
Tommaso Bianucci <churli@users.noreply.github.com>
2018-11-22 21:57:58 +0100

    Added test cases for Direction.

--- src/stencils/communicationStencils/Direction.h
+++ src/stencils/communicationStencils/Direction.h
@@ -5,9 +5,22 @@
 #ifndef NS_DIRECTION_H
 #define NS_DIRECTION_H
 
+const int NUMBER_OF_DIRECTIONS = 6;
+
 typedef enum Direction
 {
+    // In the code we always assume the values here go from 0 to NUMBER_OF_DIRECTIONS-1,
+    // order does not matter but the couples (LR, BT, FB) must stay next to each other!
     LEFT = 0, RIGHT = 1, BOTTOM = 2, TOP = 3, FRONT = 4, BACK = 5
 } Direction;
+
+namespace DirectionUtils
+{
+    unsigned char getOppositeDirection(Direction direction)
+    {
+        unsigned char u = direction & 1;
+        return ((direction>>1)<<1) + !u;
+    }
+}
 
 #endif //NS_DIRECTION_H
---
+++ test/test_main.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE "NS-EOF Unit Tests"
+
+//BOOST_AUTO_TEST_CASE(FailTest)
+//{
+//    BOOST_CHECK_EQUAL(5, 2);
+//}
+//
+//BOOST_AUTO_TEST_CASE(PassTest)
+//{
+//    BOOST_CHECK_EQUAL(4, 4);
+//}
