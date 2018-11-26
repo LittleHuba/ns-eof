622f5e9d9adc426f8e29a891c0cb63c563713821
Ulrich Huber <ulrich@huberulrich.de>
2018-11-26 11:42:33 +0100

    Add some more dependencies

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #Install mpi
-sudo apt-get install libopenmpi-dev
+sudo apt-get install libopenmpi-dev openmpi-bin
 
 #Install petsc
 wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.7.7.tar.gz
 tar -xzf petsc-3.7.7.tar.gz
 cd petsc-3.7.7
-./configure --with-cc=mpicc --with-cxx=mpicxx --with-fc=0 --download-f2cblaslapack
+./configure --with-cc=mpicc --with-cxx=mpicxx --with-fc=0 --download-f2cblaslapack --prefix=/usr/lib
 make
 #TODO can we make this path relative?
 make PETSC_DIR=/home/travis/build/LittleHuba/ns-eof/petsc-3.7.7 PETSC_ARCH=arch-linux2-c-debug test
