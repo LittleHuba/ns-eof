0cbac469beac1717b8c42597f1df01f8880233e8
Ulrich Huber <ulrich@huberulrich.de>
2018-11-26 09:43:27 +0100

    Fix some travis bugs

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-sudo apt-get install openmp
+sudo apt-get install openmp libopenmpi-dev
 ./configure --with-cc=mpicc --with-cxx=mpicxx --with-fc=0 --download-f2cblaslapack
 make
 sudo make install
