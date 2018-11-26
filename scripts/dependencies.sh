880aeafe2b31a48504d0832f75c8532842d232ce
Ulrich Huber <ulrich@huberulrich.de>
2018-11-26 13:44:31 +0100

    Use correct petsc version Fix prefix of petsc

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -4,11 +4,11 @@
 sudo apt-get install -y libopenmpi-dev openmpi-bin libboost-test-dev
 
 #Install petsc
-wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.7.7.tar.gz
-tar -xzf petsc-3.7.7.tar.gz
-cd petsc-3.7.7
-./configure --with-cc=mpicc --with-cxx=mpicxx --with-fc=0 --download-f2cblaslapack --prefix=/usr/lib
+wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.5.4.tar.gz
+tar -xzf petsc-3.5.4.tar.gz
+cd petsc-3.5.4
+./configure --with-cc=mpicc --with-cxx=mpicxx --with-fc=0 --download-f2cblaslapack --prefix=/usr/local
 make
 #TODO can we make this path relative?
-make PETSC_DIR=/home/travis/build/LittleHuba/ns-eof/petsc-3.7.7 PETSC_ARCH=arch-linux2-c-debug test
+make PETSC_DIR=/home/travis/build/LittleHuba/ns-eof/petsc-3.5.4 PETSC_ARCH=arch-linux2-c-debug test
 sudo make install
