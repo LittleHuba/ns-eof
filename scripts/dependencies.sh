94afc2c23934ada92a7edf127dce355eb894da4d
Ulrich Huber <ulrich@huberulrich.de>
2018-11-26 09:50:05 +0100

    Fix some more travis bugs

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-sudo apt-get install openmp libopenmpi-dev
+
+#Install mpi
+sudo apt-get install libopenmpi-dev
+
+#Install petsc
+wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.7.7.tar.gz
+tar -xzf petsc-3.7.7.tar.gz
+cd petsc-3.7.7
 ./configure --with-cc=mpicc --with-cxx=mpicxx --with-fc=0 --download-f2cblaslapack
 make
 sudo make install
