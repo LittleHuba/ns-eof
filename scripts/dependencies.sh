efa68f02f876bc7d00967cd17b46a88407c3ff3b
Ulrich Huber <ulrich@huberulrich.de>
2018-11-27 13:33:17 +0100

    Remove unsupported bash commands

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,16 +2,10 @@
 
 #Install petsc
 PETSC_VERSION="petsc-3.5.4"
-if [[ ! -d ${PETSC_VERSION} ]]; then
-    wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/${PETSC_VERSION}.tar.gz
-    tar -xzf ${PETSC_VERSION}.tar.gz
-    cd ${PETSC_VERSION}
-    ./configure --with-cc=mpicc --with-cxx=mpicxx --with-fc=0 --download-f2cblaslapack --prefix=/usr/local
-    make
-    #TODO can we make this path relative?
-    make PETSC_DIR=`pwd` PETSC_ARCH=arch-linux2-c-debug test
-else
-    cd ${PETSC_VERSION}
-fi
-
+wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/${PETSC_VERSION}.tar.gz
+tar -xzf ${PETSC_VERSION}.tar.gz
+cd ${PETSC_VERSION}
+./configure --with-cc=mpicc --with-cxx=mpicxx --with-fc=0 --download-f2cblaslapack --prefix=/usr/local
+make
+make PETSC_DIR=`pwd` PETSC_ARCH=arch-linux2-c-debug test
 sudo make install
