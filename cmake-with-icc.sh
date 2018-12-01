7bedf535c23878ee0ed01c4b5ef73c0a32e79395
Tommaso Bianucci <churli@users.noreply.github.com>
2018-12-01 14:46:43 +0100

    Prepared script to easily build with icc and cmake on the cluster.

---
+++ cmake-with-icc.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#if command -v module 1>/dev/null 2>&1; then
+#    # Load modules here if needed
+#fi
+
+export CC=icc
+export CXX=icpc
+
+#BUILD_TYPE="Debug"
+#BUILD_TYPE="RelWithDebInfo"
+BUILD_TYPE="Release"
+
+if [[ "$1" != "" ]]; then
+    BUILD_TYPE="$1"
+fi
+
+SRC_DIR="$(pwd)"
+BUILD_DIR="${SRC_DIR}/cmake-build-${CC}"
+TARGET="ns"
+
+mkdir -p ${BUILD_DIR}
+pushd ${BUILD_DIR}
+cmake -DPETSC_DIR=${PETSC_DIR} -DPETSC_ARCH=${PETSC_ARCH} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -G "CodeBlocks - Unix Makefiles" ${SRC_DIR}
+cmake --build ${BUILD_DIR} --target ${TARGET} -- -j 2
+popd
+ln -sf ${BUILD_DIR}/src/${TARGET} ${SRC_DIR}/.
+
+#eof
