bd265dbe26a384c5278dc3982405547762190021
Keefe Huang <huangq1234553@gmail.com>
2018-12-10 13:32:43 +0100

    Added an override function into the simulation.h

--- src/TurbulentSimulation.h
+++ src/TurbulentSimulation.h
@@ -107,6 +107,47 @@ public:
         // write flow field information to vtk file
         _turbulenceVTKStencil.write(timeStep);
     }
+
+    void setTimeStep() override
+    {
+
+        FLOAT localMin, globalMin;
+        assertion(_parameters.geometry.dim == 2 || _parameters.geometry.dim == 3);
+        FLOAT factor = 1.0 / (_parameters.meshsize->getDxMin() * _parameters.meshsize->getDxMin()) +
+                       1.0 / (_parameters.meshsize->getDyMin() * _parameters.meshsize->getDyMin());
+
+        // determine maximum velocity
+        _maxUStencil.reset();
+        _maxUFieldIterator.iterate();
+        _maxUBoundaryIterator.iterate();
+        if (_parameters.geometry.dim == 3)
+        {
+            factor += 1.0 / (_parameters.meshsize->getDzMin() * _parameters.meshsize->getDzMin());
+            _parameters.timestep.dt = 1.0 / _maxUStencil.getMaxValues()[2];
+        }
+        else
+        {
+            _parameters.timestep.dt = 1.0 / _maxUStencil.getMaxValues()[0];
+        }
+
+
+        localMin = std::min(_parameters.timestep.dt,
+                            std::min(std::min(_parameters.flow.Re / (1 + _parameters.flow.Re*_maxUStencil.getMinReynold())/ (2 * factor),
+                                              1.0 / _maxUStencil.getMaxValues()[0]),
+                                     1.0 / _maxUStencil.getMaxValues()[1]));
+
+
+
+        // Here, we select the type of operation before compiling. This allows to use the correct
+        // data type for MPI. Not a concern for small simulations, but useful if using heterogeneous
+        // machines.
+
+        globalMin = MY_FLOAT_MAX;
+        MPI_Allreduce(&localMin, &globalMin, 1, MY_MPI_FLOAT, MPI_MIN, PETSC_COMM_WORLD);
+
+        _parameters.timestep.dt = globalMin;
+        _parameters.timestep.dt *= _parameters.timestep.tau;
+    }
 };
 
 #endif //NS_TURBULENTSIMULATION_H
