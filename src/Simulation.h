15d6a23c4adf4c7489b6e1172713e61166d72feb
Ulrich Huber <ulrich@huberulrich.de>
2018-12-10 19:54:01 +0100

    remove output blocking for timestep 0

--- src/Simulation.h
+++ src/Simulation.h
@@ -195,10 +195,6 @@ public:
     /** plots the flow field. */
     virtual void plotVTK(int timeStep)
     {
-        // Skip output when timestep equals zero
-        if(timeStep == 0.0)
-            return;
-
         // iterate stencil over _flowField
         _vtkIterator.iterate();
         // write flow field information to vtk file
